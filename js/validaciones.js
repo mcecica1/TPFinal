a2ab6cc8c74fcfb29d4b00e0ccb3cc15bbef6621
mcecica1 <104572597+mcecica1@users.noreply.github.com>
2022-05-02 19:28:00 -0300

    Add files via upload

---
+++ js/validaciones.js
@@ -0,0 +1,32 @@
+function valida_envio() {
+
+   
+    if (document.contacto.Nombre.value.length == 0) {
+        alert("Tiene que escribir su nombre")
+        document.contacto.Nombre.focus()
+        return 0;
+    }
+    
+    if (document.contacto.Nombre.value.length == 0) {
+        alert("Tiene que escribir su nombre")
+        document.contacto.Nombre.focus()
+        return 0;
+    }
+    
+    if (document.contacto.Correo.value.length == 0) {
+        alert("Tiene que escribir su Correo")
+        document.contacto.Correo.focus()
+        return 0;
+    }
+
+    if (document.contacto.Telefono.value.length == 0) {
+        alert("Tiene que escribir su Telefono")
+        document.contacto.Telefono.focus()
+        return 0;
+    }
+
+    //el formulario se envia
+    alert("Muchas gracias por enviar el formulario");
+    document.contacto.submit();
+}
+
